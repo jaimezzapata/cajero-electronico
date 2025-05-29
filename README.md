72435372a451f647fac3e17dce3c7979cedbeeed
jaimezzapata <zapataval2304@gmail.com>
2025-05-28 20:53:43 -0500

    Se finaliza ejercio de cajero

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Cajero Electrónico
 - Simular el comportamiendo de un cajero electrónico teniendo en cuenta las siguientes situaciones
 
-- 1: Se debe validar un inicio de sesión (usuario y contraseña) OK
-- 2: Si hay 3 intentos fallidos de inicio de sesión, se debe finalizar la ejecuación OK
-- 3: El cajero debe permitir consultar saldo disponible en un cuenta X
-- 4: El cajero debe permitir retirar dinero (la cuenta no puede quedar en 0)
-- 5: El cajero debe permitir transferir dinero a una cuenta X (la cuenta no puede quedar en 0)
-- 6: El cajero debe permitir consigar a cuenta propia un valor (no puede ser menor 10000 y no puede superar 1000000 por transacción)
-- 7: El cajero debe tener la opción de cerrar sesión
-- 8: No se puede ingresar un valor negativo
-- 9: Mostrar un "recibo" por transacción
-- 10: Al finalizar la simulación, mostrar la cantidad de transacciones realizadas
+- 1: Se debe validar un inicio de sesión (usuario y contraseña) -> OK
+- 2: Si hay 3 intentos fallidos de inicio de sesión, se debe finalizar la ejecuación -> OK
+- 3: El cajero debe permitir consultar saldo disponible en un cuenta X -> OK
+- 4: El cajero debe permitir retirar dinero (la cuenta no puede quedar en 0) -> OK
+- 5: El cajero debe permitir transferir dinero a una cuenta X (la cuenta no puede quedar en 0) -> OK
+- 6: El cajero debe permitir consigar a cuenta propia un valor (no puede ser menor 10000 y no puede superar 1000000 por transacción) -> OK
+- 7: El cajero debe tener la opción de cerrar sesión -> OK
+- 8: No se puede ingresar un valor negativo -> "OK"
+- 9: Mostrar un "recibo" por transacción -> "OK"
+- 10: Al finalizar la simulación, mostrar la cantidad de transacciones exitosas realizadas
+- 11: Mostrar la cantidad de transacción erroneas (restar transacción exitosas del total de transacciones)
--- src/App.java
+++ src/App.java
@@ -11,9 +11,75 @@ public class App {
             String contrasena = sc.nextLine();
             if (usuario.equals("admin") && contrasena.equals("1234")) {
                 System.out.println(usuario + ". Bienvenido al Cajero electrónico");
-                double saldoCuenta = 0;
-                System.out.println("Seleccione una opción: \n1 - Consultar Saldo\n2 - Retirar Dinero\n3 - Depositar Dinero\n4 - Transferir Dinero\n5 - Cerrar Sesión");
-                int opcion = sc.nextInt();
+                double saldoCuenta = 11000;
+                double valorRetiro = 0;
+                int transacciones = 0;
+                int opcion = 0;
+                do {
+                    System.out.println(
+                            "Seleccione una opción: \n1 - Consultar Saldo\n2 - Retirar Dinero\n3 - Transferir Dinero\n4 - Depositar Dinero\n5 - Cerrar Sesión");
+                    opcion = sc.nextInt();
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Su saldo es: " + saldoCuenta);
+                            transacciones++;
+                            break;
+                        case 2:
+                            System.out.print("Ingrese el valor a retirar: ");
+                            valorRetiro = sc.nextDouble();
+                            if (valorRetiro >= saldoCuenta) {
+                                System.out
+                                        .println("El valor " + valorRetiro + " supera el valor que tiene en la cuenta");
+                            } else {
+                                if (saldoCuenta - valorRetiro < 10000) {
+                                    System.out.println("La cuenta no puede tener menos de 10000");
+                                } else {
+                                    saldoCuenta -= valorRetiro;
+                                    System.out.println("Retiro exitoso. Su nuevo saldo es: " + saldoCuenta);
+                                    transacciones++;
+                                }
+                            }
+                            break;
+                        case 3:
+                            System.out.print("Ingrese el valor a transferir: ");
+                            valorRetiro = sc.nextDouble();
+                            System.out.println("Ingrese el número de cuenta al que desea transferir: ");
+                            if (valorRetiro >= saldoCuenta) {
+                                System.out
+                                        .println("El valor " + valorRetiro + " supera el valor que tiene en la cuenta");
+                            } else {
+                                if (saldoCuenta - valorRetiro < 10000) {
+                                    System.out.println("La cuenta no puede tener menos de 10000");
+                                } else {
+                                    saldoCuenta -= valorRetiro;
+                                    System.out.println("Tranferencia exitosa. Su nuevo saldo es: " + saldoCuenta);
+                                    transacciones++;
+                                }
+                            }
+                            break;
+                        case 4:
+                            System.out.print("Ingrese el valor a consignar: ");
+                            double valorConsignacion = sc.nextDouble();
+                            if (valorConsignacion < 0) {
+                                System.out.println("No puede ingresar valores negativos");
+                            } else if (valorConsignacion < 10000) {
+                                System.out.println("El valor minimo a depositar es 10000");
+                            } else if (valorConsignacion > 1000000) {
+                                System.out.println("El valor a despositar no puede ser superior a 1000000");
+                            } else {
+                                saldoCuenta += valorConsignacion;
+                                System.out.println("Consignación exitosa. Su nuevo saldo es: " + saldoCuenta);
+                                transacciones++;
+                            }
+                            break;
+                        case 5:
+                            System.out.println("Cerrando sesión...");
+                            intentos = 3;
+                            break;
+                    }
+                } while (opcion != 5);
+                System.out.println("El día de hoy se hicieron " + transacciones
+                        + " transacciones. Gracias por usar el cajero electrónico.");
             } else {
                 System.out.println("Usuario y/o contraseña incorrectos");
                 intentos++;
